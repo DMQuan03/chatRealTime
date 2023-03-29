a2373d57dc62e71259e64113f2171372c425acab
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2023-03-29 21:40:39 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useRef, useState } from "react";
+import io from "socket.io-client"
+import { useEffect } from "react"
+import Login from "./login/login";
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
   return (
-    <div className="App">
-      APP
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: 600 }}>
+      <Login />
     </div>
-  );
+  )
+
 }
 
 export default App;
